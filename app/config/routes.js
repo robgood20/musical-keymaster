511cd7000060fe71a90c922d53e7d35c4fa334cf
Bert <Bert>
2016-02-18 13:59:19 -0700

    broken trying to redirect to home.html

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,12 @@
-// var app = angular.module('KMRouter', ["ui.router"]);
+
 
 app.config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/home');
     
     $stateProvider.state('home', {
         url: '/home',
-        template: '<h1> Hi <h1>',
-       // templateURL: 'app/config/home.html',
+        // template: '<h1> Hi <h1>',
+       templateURL: 'app/components/home/home.html',
         controller: 'HomeController',
         controllerAs: 'hc'
     })
